0738e282cb19e393a1023164c3105608762b9b4c
jfbourdon <jean.francois.bourdon@gmail.com>
2026-04-13 16:12:20 -0400

    Integration du calcul du D8 à même la fonction du downslope

--- whitebox-tools-app/src/tools/terrain_analysis/wetness_index_boehner_and_conrad.rs
+++ whitebox-tools-app/src/tools/terrain_analysis/wetness_index_boehner_and_conrad.rs
@@ -886,7 +886,8 @@ fn get_twi<'a>(twi: &'a mut Raster, m_amod: Array2D<f32>, m_slope: Array2D<f32>,
 
 
 
-fn get_d8<'a>(dem: &'a Arc<Raster>) -> Array2D<i16> {
+
+fn get_downslope_index<'a>(dem: &'a Raster, max_drop:f64, max_dist:f64) -> Array2D<f32> {
     let rows = dem.configs.rows as isize;
     let columns = dem.configs.columns as isize;
     let nodata = dem.configs.nodata;
@@ -894,15 +895,6 @@ fn get_d8<'a>(dem: &'a Arc<Raster>) -> Array2D<i16> {
     let cell_size_y = dem.configs.resolution_y;
     let diag_cell_size = (cell_size_x * cell_size_x + cell_size_y * cell_size_y).sqrt();
 
-    let out_nodata = -1i16;
-    let mut d8: Array2D<i16> = Array2D::new(rows, columns, out_nodata, out_nodata).unwrap();
-
-    let num_procs = num_cpus::get() as isize;
-    let (tx, rx) = mpsc::channel();
-    for tid in 0..num_procs {
-        let dem = dem.clone();
-        let tx1 = tx.clone();
-        thread::spawn(move || {
             let d_x = [1, 1, 1, 0, -1, -1, -1, 0];
             let d_y = [-1, 0, 1, 1, 1, 0, -1, -1];
             let grid_lengths = [
@@ -916,12 +908,20 @@ fn get_d8<'a>(dem: &'a Arc<Raster>) -> Array2D<i16> {
                 cell_size_y,
             ];
 
-            let out_vals = [1i16, 2, 4, 8, 16, 32, 64, 128];  // WBT pointer style
-            // Ultimement, mettre en i8 et conserver les indices plutôt que les valeurs
 
+    // Compute D8 Flow Direction pointer array
+    let fdir_noflow = -1_i8;
+    let mut d8: Array2D<i8> = Array2D::new(rows, columns, fdir_noflow, fdir_noflow).unwrap();
+
+    let num_procs = num_cpus::get() as isize;
+    let (tx, rx) = mpsc::channel();
+    for tid in 0..num_procs {
+        let dem = dem.clone();
+        let tx1 = tx.clone();
+        thread::spawn(move || {
             let (mut z, mut z_n, mut slope): (f64, f64, f64);
             for row in (0..rows).filter(|r| r % num_procs == tid) {
-                let mut data = vec![out_nodata; columns as usize];
+                let mut data = vec![fdir_noflow; columns as usize];
                 for col in 0..columns {
                     z = dem[(row, col)];
                     if z != nodata {
@@ -938,9 +938,7 @@ fn get_d8<'a>(dem: &'a Arc<Raster>) -> Array2D<i16> {
                             }
                         }
                         if max_slope >= 0f64 {
-                            data[col as usize] = out_vals[dir];
-                        } else {
-                            data[col as usize] = 0i16;
+                            data[col as usize] = dir as i8;
                         }
                     }
                 }
@@ -950,111 +948,73 @@ fn get_d8<'a>(dem: &'a Arc<Raster>) -> Array2D<i16> {
     }
 
 
-    for row in 0..rows {
+    for _ in 0..rows {
         let data = rx.recv().expect("Error receiving data from thread.");
         d8.set_row_data(data.0, data.1);
     }
 
 
-    return d8;
-
-}
 
 
+    // Compute Downslope Index array
+    let downslope_nodata = 0f32;
+    let mut downslope_index: Array2D<f32> = Array2D::new(rows, columns, downslope_nodata, downslope_nodata).unwrap();
 
 
-
-fn get_downslope_index<'a>(dem: &'a Raster, d8: &'a Array2D<i16>, row: isize, col: isize, max_drop:f64, max_dist:f64) -> f32 {
-    let nodata = dem.configs.nodata;
-    let rows = dem.configs.rows;
-    let columns = dem.configs.columns;
-    let cell_size_x = dem.configs.resolution_x;
-    let cell_size_y = dem.configs.resolution_y;
-    let diag_cell_size = (cell_size_x * cell_size_x + cell_size_y * cell_size_y).sqrt();
-
-    let fdir_nodata = d8.nodata;
-
-
-    let d_x = [1, 1, 1, 0, -1, -1, -1, 0];
-    let d_y = [-1, 0, 1, 1, 1, 0, -1, -1];
-    let grid_lengths = [
-        diag_cell_size,
-        cell_size_x,
-        diag_cell_size,
-        cell_size_y,
-        diag_cell_size,
-        cell_size_x,
-        diag_cell_size,
-        cell_size_y,
-    ];
-
-
-    let mut pntr_matches: [usize; 129] = [0usize; 129];
-    pntr_matches[1] = 0usize;
-    pntr_matches[2] = 1usize;
-    pntr_matches[4] = 2usize;
-    pntr_matches[8] = 3usize;
-    pntr_matches[16] = 4usize;
-    pntr_matches[32] = 5usize;
-    pntr_matches[64] = 6usize;
-    pntr_matches[128] = 7usize;
-
-
-
-
-    // Create a list to store values for distance and elevation reach by algorithm (sum_dist_list, sum_drop_list), and initialise value (0)
-    let mut sum_dist_list = Vec::<(f64)>::with_capacity(rows + columns);
-    let mut sum_drop_list = Vec::<(f64)>::with_capacity(rows + columns);
+    for row in 0..rows {
+        for col in 0..columns {
+            let z = dem.get_value(row, col);
+            if z != nodata {
+                // Create a list to store values for distance and elevation reach by algorithm
+                // (sum_dist_list, sum_drop_list), and initialise value (0)
+                let mut sum_dist_list = Vec::<f64>::with_capacity((rows + columns) as usize);
+                let mut sum_drop_list = Vec::<f64>::with_capacity((rows + columns) as usize);
     let mut sum_dist = 0f64;
     let mut sum_drop = 0f64;
 
-    // Get elevation of DEM pixel and skip NoData
-    let z = dem.get_value(row, col);
-    if z == nodata {
-        return 0f32;
-    }
-
-
     // Extract starting point where the algorithm will move
-    let mut last_row = row;
-    let mut last_col = col;
+                let mut row_n = row;
+                let mut col_n = col;
 
 
-    // Descente le long du D8
+                // Follow D8 Pointer downstream until limits are reached
     while sum_dist <= max_dist && sum_drop <= max_drop {
-        // Get the corresponding flow direction pixel
-        let fdir = d8.get_value(last_row, last_col);
-
-        // If flow direction value correspond to NoData or NoFlow,
-        // stop iteration as the end of flow has been reached
-        if fdir <= 0i16 {
+                    // Get the corresponding flow direction pixel and
+                    // break out of loop if we are on a NoFlow cell
+                    let fdir_idx = d8.get_value(row_n, col_n);
+                    if fdir_idx == fdir_noflow {
             break;
         }
 
-        // Get the line of the dictionary to get flow direction of current pixel
-        let idx = pntr_matches[fdir as usize];
-
         // Move the position of pixel according to flow direction
-        last_row += d_y[idx];
-        last_col += d_x[idx];
+                    row_n += d_y[fdir_idx as usize];
+                    col_n += d_x[fdir_idx as usize];
 
         // Compute the difference of elevation between initial DEM elevation and current DEM elevation
-        sum_drop = z - dem.get_value(last_row, last_col);
+                    sum_drop = z - dem.get_value(row_n, col_n);
         sum_drop_list.push(sum_drop);
 
         // Compute the distance by flow direction between initial DEM pixel and current DEM pixel
-        sum_dist += grid_lengths[idx];
+                    sum_dist += grid_lengths[fdir_idx as usize];
         sum_dist_list.push(sum_dist);
     }
 
 
+                if sum_dist_list.len() > 0 {
     let mut idx = sum_dist_list.len() as usize;
     if sum_drop > max_drop {
-        idx = ((idx).div_euclid(2) - 1) as usize
+                        idx = ((idx as f64 / 2f64).floor() - 1f64) as usize
+                    } else {
+                        idx -= 1 as usize;
     }
 
+                    downslope_index.set_value(row, col, (sum_drop_list[idx] / sum_dist_list[idx] * 100_f64) as f32);
+                }
 
-    let downslope_index = (sum_drop_list[idx] / sum_dist_list[idx] * 100_f64) as f32;
+            }
+        }
+    }
+   
 
     return downslope_index;
 }
